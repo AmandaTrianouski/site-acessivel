bed5239d5eb8e2186100d7588addb6ed251af8c5
AmandaTrianouski <160858147+AmandaTrianouski@users.noreply.github.com>
2025-04-04 08:28:23 -0300

    gjf

--- script2.js
+++ script2.js
@@ -1,26 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
-    
-    botaoDeAcessibilidade.addEventListener('click', function () {
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista'); 
-    
-    })
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+     const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+     botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+     
+     }) 
+
+    let tamanhoAtualFonte = 1;
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    aumentaFonteBotao.addEventListener('click', function(){
+     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+         aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        botaoDeAcessibilidade.addEventListener('click', function () {
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-    })
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
-
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
     })
     })
